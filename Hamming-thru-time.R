7e4a2a1ad9f1238b741c02604c12e56ed9337a4f
Sergei Tarasov <sergxf@yandex.ru>
2025-11-07 15:39:12 +0200

    ham

---
+++ Hamming-thru-time.R
@@ -0,0 +1,84 @@
+library(rphenoscate)
+library(expm)
+library(stringdist)
+
+Q1 <- initQ(c(0, 1), c(1,1))
+
+Q <- amaSMM(Q1,Q1)
+
+Q
+start.state <- 1
+expm(Q)
+
+
+# Function to compute transition matrix for a time-varying rate multiplier
+P_time_varying <- function(Q, rate_fun, t0, t1) {
+  # Integrate rate over [t0, t1]
+  R <- integrate(rate_fun, lower = t0, upper = t1)$value
+  
+  # Matrix exponential of Q scaled by integrated rate
+  P <- expm(Q * R)
+  return(P)
+}
+
+
+hamming_var_from_P_exact <- function(P, start_state = 1) {
+  # Ensure matrix and names
+  stopifnot(is.matrix(P))
+  states <- colnames(P)
+  probs  <- P[start_state, ]
+  
+  # Compute pairwise Hamming distance matrix
+  D <- stringdistmatrix(states, states, method = "hamming")
+  D <- as.matrix(D)
+  
+  # Expected Hamming distance
+  E_D  <- sum(outer(probs, probs) * D)
+  
+  # Expected squared Hamming distance
+  E_D2 <- sum(outer(probs, probs) * D^2)
+  
+  # Variance
+  Var_D <- E_D2 - E_D^2
+  
+  Var_D
+  # list(
+  #   mean_hamming = E_D,
+  #   var_hamming  = Var_D
+  # )
+}
+
+# Example -----------------------------------------------------------
+
+
+# Define a time-varying rate function, e.g., an exponential decay
+# rate_fun <- function(t) {
+#   0.55 + 0.45 * sin(2 * pi * 3 * t / 5)  # 3 full oscillations between 0.1 and 1
+# }
+rate_fun <- function(t) {
+  0.1 + 0.9 * sin(pi * t / 5)
+}
+x <- seq(0, 10, .1)
+y <- rate_fun(x)
+plot(x,y, type='l')
+ 
+# Compute transition probabilities from time 0 to 200
+P <- P_time_varying(Q, rate_fun, 0, .1)
+P
+hamming_var_from_P_exact(P, start_state = 1)
+
+library(purrr)
+
+# --- Compute variance over time ---
+time_points <- seq(0, 10, by = 0.01)
+
+var_results <- map_dfr(time_points, function(t) {
+  P <- P_time_varying(Q*.01, rate_fun, 0, t)
+  res <- hamming_var_from_P_exact(P, start_state = 1)
+  tibble(time = t, var=res)
+})
+
+# inspect
+head(var_results)
+plot(var_results$time, var_results$var, type='l')
+lines(x,y*.1, col='red')
--- script_disparity_plot.R
+++ script_disparity_plot.R
@@ -5,184 +5,183 @@ library(ontophylo)
 library(tidyverse)
 
 # Load data.
-load("data_out/paramo_stm_adult_final.RDA")
-load("data_out/paramo_amalg_adult.RDA")
+# load("data_out/paramo_stm_adult_final.RDA")
+# load("data_out/paramo_amalg_adult.RDA")
 
 # Set some parameters.
-res = 200
+nmaps = 1000
 n_samples = 100
+res = 1000
+idx_s = seq(from = 1, to = nmaps, by = n_samples)
+idx_e = tail(seq(from = 0, to = nmaps, by = n_samples), -1)
 
 #--------------------------------------------------#
 
 ### INITIAL ORGANIZATION ###
 
 # Get global timings.
+hym_tree <-readRDS("data/hym_tree.RDS")
 hym_tree_discr <- discr_Simmap(hym_tree, res)
 node_heights <- nodeHeights(hym_tree_discr)[,1]
+#nodeHeights(hym_tree)[,1]
 br_sums <- lapply(hym_tree_discr$maps, function(x) cumsum(x) )
-times <- mapply(x = node_heights, y = br_sums, function(x,y) round((x + y),4) )
+times <- mapply(x = node_heights, y = br_sums, function(x,y) round((x + y),5) )
+times
 
-# Import first stochastic map.
-stm <- readRDS(paste0("stmaps/CH1.RDS"))
+times[1]
 
-# Get a sample of stochastic maps.
-stm <- stm[seq((length(stm)/n_samples), length(stm), (length(stm)/n_samples))]
+# Create a folder to store all temporary maps.
+dir.create("stmaps_disp")
 
-# Discretize first stochastic map.
-stm_discr <- discr_Simmap_all(stm, res)
-
-# Extract all maps.
-br_maps <- lapply(stm_discr, function(x) lapply(x$maps, function(y) names(y) ) )
-
-# Set starting map.
-smaps <- br_maps
-
-# Get character IDs.
-CH_IDs <- hym_annot_final$char_id
-
-#--------------------------------------------------#
-
-# Loop over all remaining characters.
-for (i in 2:length(CH_IDs)) {
+# Loop over all map samples.
+for (i in 1:length(idx_s)) {
   
-  # Import stochastic map.
-  stm <- readRDS(paste0("stmaps/", CH_IDs[[i]], ".RDS"))
+  # Import first stochastic map.
+  stm <- readRDS(paste0("stmaps/CH1.RDS"))
   
   # Get a sample of stochastic maps.
-  stm <- stm[seq((length(stm)/n_samples), length(stm), (length(stm)/n_samples))]
+  stm <- stm[idx_s[i]:idx_e[i]]
   
-  # Discretize stochastic map.
+  # Discretize first stochastic map.
   stm_discr <- discr_Simmap_all(stm, res)
   
   # Extract all maps.
   br_maps <- lapply(stm_discr, function(x) lapply(x$maps, function(y) names(y) ) )
   
-  cat(paste0("\n", "Working on: ", CH_IDs[[i]], " ", Sys.time(), "\n"))
+  # Set starting map.
+  smaps <- br_maps
   
-  # Concatenate all branch maps for a new character across all stochastic maps. 
-  for (j in 1:n_samples) {
+  # Get character IDs.
+  CH_IDs <- hym_annot_final$char_id
+  
+  #--------------------------------------------------#
+  
+  # Loop over all remaining characters.
+  for (j in 2:length(CH_IDs)) {
     
-    smaps[[j]] <- mapply(x = smaps[[j]], y = br_maps[[j]], function(x,y) rbind(x,y) )
+    # Import stochastic map.
+    stm <- readRDS(paste0("stmaps/", CH_IDs[[j]], ".RDS"))
+    
+    # Get a sample of stochastic maps.
+    stm <- stm[idx_s[i]:idx_e[i]]
+    
+    # Discretize stochastic map.
+    stm_discr <- discr_Simmap_all(stm, res)
+    
+    # Extract all maps.
+    br_maps <- lapply(stm_discr, function(x) lapply(x$maps, function(y) names(y) ) )
+    
+    cat(paste0("\n", "Working on: ", CH_IDs[[j]], " ", Sys.time(), "\n"))
+    
+    # Concatenate all branch maps for a new character across all stochastic maps. 
+    for (k in 1:n_samples) {
+      
+      smaps[[k]] <- mapply(x = smaps[[k]], y = br_maps[[k]], function(x,y) rbind(x,y) )
+      
+    }
     
   }
   
-}
-
-# Merge all individual states as single string.
-smaps_merg <- lapply(smaps, function(x) lapply(x, function(y) apply(y,2,paste0, collapse = "") ) )
-
-# Save temporary data object.
-#saveRDS(smaps_merg, "data_out/smaps_disparity.RDS")
-#smaps_merg <- readRDS("data_out/smaps_disparity.RDS")
-
-# OBS.: alternatively one can simply import the amalgamated stochastic map and extract the mappings!
-
-# Clean workspace.
-rm(i, j, stm, stm_discr, smaps)
-
-# Plot tree to check branch and node labels.
-plot.phylo(hym_tree, show.tip.label = F)
-edgelabels(frame = "none", col = "red", cex = 0.6)
-nodelabels(frame = "none", col = "blue", cex = 0.6)
-
-# Add timings to all amalgamated states.
-smaps_final <- vector(mode = "list", length = n_samples)
-for (i in 1:n_samples) { smaps_final[[i]] <- mapply(x = times, y = smaps_merg[[i]], function(x,y) setNames(x,y) ) }
-
-# Pre-allocate list to store all Hamming distances.
-hm_dist_all <- vector(mode = "list", length = n_samples)
-
-# Loop over all stochastic maps.
-for (j in 1:n_samples) {
+  # Merge all individual states as single string.
+  smaps_merg <- lapply(smaps, function(x) lapply(x, function(y) apply(y,2,paste0, collapse = "") ) )
   
-  cat(paste0("\n", "Working on Map: ", j, " ", Sys.time(), "\n"))
+  # Save temporary data object.
+  saveRDS(smaps_merg, paste0("stmaps_disp/smaps_disp_", i, ".RDS"))
   
-  # Extract all bin groups for a given amalgamated stochastic map.
-  t_bins <- split(sort(unlist(smaps_final[[j]])), f = as.factor(sort(unlist(smaps_final[[j]]))))
-  
-  # Remove singletons bins (due to approximations when discretizing maps).
-  t_bins_clean <- t_bins[sapply(t_bins, function(x) length(x) > 1 )]
-  
-  # Calculate Hamming distances among all amalgamated states for each time bin.
-  hm_dist_all[[j]] <- sapply(t_bins_clean, function(x) stringdist::stringdistmatrix(names(x), names(x), method = "hamming") %>% mean )
+  # Clean workspace.
+  rm(j, k, stm, stm_discr, smaps)
   
 }
 
-# Get number of lineages present at each time bin.
-ltt <- sapply(t_bins_clean, length)
+#------------------------------------------
 
-# Quick checks.
-apply(do.call(cbind, hm_dist_all),1,summary)
+# Re-Import all map samples.
+k=1
+for (k in 1:(nmaps/n_samples)) {
+  
+  cat(paste0("\n", "Working on sample: ", k, " ", Sys.time(), "\n"))
+  
+  # Import raw disparity information.
+  smaps_merg <- readRDS(paste0("stmaps_disp/smaps_disp_", k, ".RDS"))
+  length(smaps_merg)
+  
+  # Add timings to all amalgamated states.
+  smaps_final <- vector(mode = "list", length = n_samples)
+  i=1
+  for (i in 1:n_samples) { 
+    smaps_final[[i]] <- mapply(x = times, y = smaps_merg[[i]], function(x,y) setNames(x,y) ) 
+    }
+  
+  # Pre-allocate list to store all Hamming distances.
+  hm_dist_all <- vector(mode = "list", length = n_samples)
+  
+  # Loop over all stochastic maps.
+  j=1
+  for (j in 1:n_samples) {
+    
+    cat(paste0("\n", "Working on Map: ", j, " ", Sys.time(), "\n"))
+    
+    # Extract all bin groups for a given amalgamated stochastic map.
+    t_bins <- split(sort(unlist(smaps_final[[j]])), f = as.factor(sort(unlist(smaps_final[[j]]))))
+    
+    # Remove singletons bins (due to approximations when discretizing maps).
+    t_bins_clean <- t_bins[sapply(t_bins, function(x) length(x) > 1 )]
+    
+    # Calculate Hamming distances among all amalgamated states for each time bin.
+    hm_dist_all[[j]] <- sapply(t_bins_clean, function(x) stringdist::stringdistmatrix(names(x), names(x), method = "hamming") %>% mean )
+    
+  }
+  
+  # Get number of lineages present at each time bin.
+  ltt <- sapply(t_bins_clean, length)
 
-# Build tibble for plotting (already adding normalized disparity).
-#tb_dispar <- lapply(hm_dist_all, function(x) tibble(Time = c(0, as.numeric(names(x))), 
-#                                                    Disparity = c(0, round(unname(x),2)),
-#                                                    Norm_Disparity = c(0, round(unname(x)/ltt,2))) )
+  # Build tibble for plotting (already adding normalized disparity).
+  tb_dispar <- lapply(hm_dist_all, function(x) tibble(Time = c(0, as.numeric(names(x))), 
+                                                      Disparity = c(0, round(unname(x),2)),
+                                                      Norm_Disparity = c(0, round(unname(x)/ltt,2)),
+                                                      N_Lineages = c(1,ltt) ) )
+  # Save temporary data object.
+  saveRDS(tb_dispar, paste0("data_out/data_disp_", k, ".RDS"))
+  
+}
 
-# Build tibble for plotting.
-tb_dispar <- lapply(hm_dist_all, function(x) tibble(Time = c(0, as.numeric(names(x))), 
-                                                    Disparity = c(0, round(unname(x),2))) )
-tb_dispar <- do.call(bind_rows, tb_dispar)
-tb_dispar
+i = 1
+# Re-import all tibbles.
+tb_dispar <- list()
+for (i in 1:(nmaps/n_samples)) { tb_dispar <- c(tb_dispar, readRDS(paste0("data_out/data_disp_", i, ".RDS"))) }
+
+# Revert time axis. Calculate CI. Build final tibble.
+tb_dispar_summ <- tibble("Time" = apply(sapply(tb_dispar, function(x) rev(x$Time) ), 1, median),
+                         "Median_disparity" = apply(sapply(tb_dispar, function(x) x$Disparity ), 1, median),
+                         "Mean_disparity" = apply(sapply(tb_dispar, function(x) x$Disparity ), 1, mean),
+                         "Low_CI_disparity" = apply(sapply(tb_dispar, function(x) x$Disparity ), 1, function(y) quantile(y, 0.025, na.rm = TRUE)),
+                         "High_CI_disparity" = apply(sapply(tb_dispar, function(x) x$Disparity ), 1, function(y) quantile(y, 0.975, na.rm = TRUE)),
+                         "Median_norm_disparity" = apply(sapply(tb_dispar, function(x) x$Norm_Disparity ), 1, median),
+                         "Mean_norm_disparity" = apply(sapply(tb_dispar, function(x) x$Norm_Disparity ), 1, mean),
+                         "Low_CI_norm_disparity" = apply(sapply(tb_dispar, function(x) x$Norm_Disparity ), 1, function(y) quantile(y, 0.025, na.rm = TRUE)),
+                         "High_CI_norm_disparity" = apply(sapply(tb_dispar, function(x) x$Norm_Disparity ), 1, function(y) quantile(y, 0.975, na.rm = TRUE)),
+                         "N_Lineages" = apply(sapply(tb_dispar, function(x) rev(x$N_Lineages) ), 1, mean) )
 
 # Save temporary data object.
-#saveRDS(tb_dispar, "data_out/data_disparity.RDS")
-#tb_dispar <- readRDS("data_out/data_disparity.RDS")
-
-# Summarize data and add CI.
-tb_dispar_summ <- tb_dispar %>% group_by(Time) %>% 
-  
-  summarise(Mean_Disparity = mean(Disparity), se = sd(Disparity)/sqrt(n()), .groups = "drop") %>% 
-  
-  mutate(CI_lower = Mean_Disparity - 1.96 * se, CI_upper = Mean_Disparity + 1.96 * se)
-
-# Add column normalized by number of lineages.
-tb_dispar_summ <- tb_dispar_summ %>% mutate(Norm_Disparity = Mean_Disparity/c(1,ltt))
-tb_dispar_summ %>% mutate(N_lineages = c(1, ltt))
-
-
-
-#------------------------------##------------------------------##------------------------------##------------------------------#
-# Save temporary data object.
-#saveRDS(tb_dispar_summ, "data_out/data_disparity_final.RDS")
+saveRDS(tb_dispar_summ, "data_out/data_disparity_final.RDS")
 tb_dispar_summ <- readRDS("data_out/data_disparity_final.RDS")
 tb_dispar_summ
-colnames(tb_dispar_summ)
 
-plot(tb_dispar_summ$Time, tb_dispar_summ$Mean_norm_disparity, type='l', xlim=c(280,0))
-
-plot(tb_dispar_summ$Time, tb_dispar_summ$Mean_disparity, type='l', xlim=c(280,0))
-
-plot(tb_dispar_summ$Time, tb_dispar_summ$N_Lineages, type='l', xlim=c(280,0))
-
-
-
-plot(tb_dispar_summ$Time, rev(tb_dispar_summ$N_Lineages), type='l', xlim=c(280,0))
-lines(tb_dispar_summ$Time, tb_dispar_summ$Mean_norm_disparity*10, col='red')
-
-max(tb_dispar_summ$CI_upper)
-plot(tb_dispar_summ$Time, tb_dispar_summ$Mean_norm_disparity, type='l', ylim=c(0, 20), , xlim=c(280,0))
-lines(tb_dispar_summ$Time, tb_dispar_summ$Low_CI_norm_disparity, col='red')
-lines(tb_dispar_summ$Time, tb_dispar_summ$High_CI_norm_disparity, col='blue')
 #------------------------------#
 # NORMALIZED DISPARITY #
 #------------------------------#
 
 # Make base plot.
-base_plot <- tb_dispar_summ %>% ggplot(aes(x = Time, y =  Norm_Disparity)) + 
+base_plot <- tb_dispar_summ %>% ggplot(aes(x = Time, y =  Mean_norm_disparity)) + 
   
-  #geom_smooth(method = "loess", linewidth = 0.1, color = "#B5EAD7", se = TRUE, fill = "#FFDAC1", alpha = 0.4) +
-  #geom_smooth(method = "lm", formula = y ~ x + 0, linewidth = 0.1, color = "#B5EAD7", se = TRUE, fill = "#FFDAC1", alpha = 0.4) +
-  geom_smooth(method = "gam", formula = y ~ s(x, bs = "cs"), linewidth = 0.1, color = "#B5EAD7", se = TRUE, fill = "#FFDAC1", alpha = 0.4) +
+  geom_ribbon(data = tb_dispar_summ, aes(x = Time, ymin = Low_CI_norm_disparity, ymax = High_CI_norm_disparity), fill = "blue", alpha = 0.2) +
   
-  #geom_ribbon(aes(ymin = CI_lower, ymax = CI_upper), fill = "#FFDAC1", alpha = 0.8) + 
-  #geom_ribbon(aes(ymin = CI_lower, ymax = CI_upper), fill = "black", alpha = 0.8) +
-  
-  #geom_line(linewidth = 1.2, color = "#B5EAD7") + 
+  geom_line(data = tb_dispar_summ, aes(x = Time, y = Mean_norm_disparity), color = "black", linewidth = 0.8) +
   
   # Custom scale. Adjust if necessary.
-  scale_x_continuous(breaks = c(seq(0, max(tb_dispar_summ$Time), by = 50), max(tb_dispar_summ$Time)),
-                     labels = c(round(seq(max(tb_dispar_summ$Time), 0, by = -50),0), 0)) +
+  scale_x_reverse(breaks = seq(0, 280, by = 50)) +
+  #scale_x_continuous(breaks = c(seq(0, max(tb_dispar_summ$Time), by = 50), max(tb_dispar_summ$Time)),
+  #                   labels = c(round(seq(max(tb_dispar_summ$Time), 0, by = -50),0), 0)) +
   
   theme_minimal(base_size = 14) + 
   
@@ -193,4 +192,5 @@ base_plot <- tb_dispar_summ %>% ggplot(aes(x = Time, y =  Norm_Disparity)) +
 
 # Save plot.
 base_plot
-#ggsave("disparity_through_time_plot.png", width = 12, height = 8, dpi = 300, bg = "white")
+
+ggsave("disparity_through_time_plot.png", width = 12, height = 8, dpi = 300, bg = "white")
